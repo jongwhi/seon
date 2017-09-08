671e0a306a5b1ef22155ca8c8eb5aee6044659a5
seon <seon@github.com>
2017-09-08 13:07:15 +0900

    new1

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("대가리");
         System.out.println("겁나큼 ㅋㅋㅋ");
         System.out.println("머리크면 머하냐 멍청한데");
+        System.out.println("나도....");
     }
 }
