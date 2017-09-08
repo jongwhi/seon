2c4f489802f7509aaffb2717e1cd8e815eee982f
seon <seon@github.com>
2017-09-08 13:06:19 +0900

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("박진수");
+        System.out.println("대가리");
+        System.out.println("겁나큼 ㅋㅋㅋ");
+        System.out.println("머리크면 머하냐 멍청한데");
+    }
+}
